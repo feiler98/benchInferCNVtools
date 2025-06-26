484a0e2ca742455557337d9ff28a0f096f63c590
feiler98 <werner.feiler@ukmuenster.de>
2025-06-26 15:36:36 +0200

    add dependencies

---
+++ requirements.R
@@ -0,0 +1,12 @@
+# pseudo requirements.txt format
+# repository: https://github.com/joundso/requirements
+
+# installation on CRAN | type "R" in terminal
+# install.packages("requiRements")
+
+install_packages <- "
+infercnv
+devtools
+"
+requiRements::install(packages = install_packages)
+install_github("navinlabcode/copykat")
